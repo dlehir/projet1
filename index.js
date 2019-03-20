c4762709a676928caf181253120d9f2f3725ec9a
Dylan Le Hir <lehir-dylan@hotmail.com>
2019-03-20 20:07:43 +0100

    Suppression index JS

--- index.js
+++
@@ -1,6 +0,0 @@
-let allChamps = document.getElementsByClassName("champs");
-console.log(typeof allChamps);
-for (let i = 0 ; i < allChamps.length ; i++) {
-    allChamps[i].style.backgroundColor = 'yellow';
-}
-const hello = "hahah";
