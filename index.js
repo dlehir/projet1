41d64c66005751edd0c1bc1be5826606dfea4cde
Dylan Le Hir <lehir-dylan@hotmail.com>
2019-03-20 20:05:33 +0100

    changement nom dossier

---
+++ index.js
@@ -0,0 +1,6 @@
+let allChamps = document.getElementsByClassName("champs");
+console.log(typeof allChamps);
+for (let i = 0 ; i < allChamps.length ; i++) {
+    allChamps[i].style.backgroundColor = 'yellow';
+}
+const hello = "hahah";
